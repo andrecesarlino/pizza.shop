d396c082e49da205051c5a8e9639089a0284cbcd
andre <andreclino@gmail.com>
2024-02-03 11:26:54 -0300

    primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './global.css'
+
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
+
+import { router } from './routes'
+
+function App() {
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <Toaster richColors />
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  )
+}
+
+export default App
