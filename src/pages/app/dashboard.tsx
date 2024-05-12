766211238df04e1ea34c163b72c2cb12c8276f67
Andre Cesar Lino <andreclino@gmail.com>
2024-05-12 11:07:05 -0300

    criado o navbar

--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -1,8 +1,11 @@
 import { Helmet } from 'react-helmet-async'
 
+import { Header } from '@/components/header'
+
 export function Dashboard() {
   return (
     <>
+      <Header />
       <Helmet title="Dashboard" />
       <h1>teste</h1>
     </>
